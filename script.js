953548737b36355c9cc254915cf59ddd5082159b
Manoj Rijal <maanojriijal@gmail.com>
2019-02-25 22:24:14 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+//var button = document.getElementsByTagName("button")[0]
+//
+//button.addEventListener("click", function() {
+//    console.log("CLICK !!!");
+//})
+
+//button.addEventListener("mouseenter", function() {
+//    console.log("CLICK !!!!");
+//})
+
+//button.addEventListener("mouseleave", function() {
+//    console.log("CLICK !!!");
+//})
+
+//button.addEventListener("click", function() {
+//    console.log("Click is working");
+//})
+
+
+
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+
+function inputLength(){
+    return input.value.length;
+}
+
+function createListElement(){
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+        input.value="";
+}
+
+function addListAfterClick(){
+     if (inputLength() > 0) {
+       createListElement();
+    }
+}
+
+function addListAfterKeypress(event){
+     if (inputLength()> 0 && event.which === 13) {
+        createListElement();
+    }
+    
+}
+
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
